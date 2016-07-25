fd5d5326f15c7133075e6f80ab9300c1df24b26b
liujianchang <jianchangliu@yeah.net>
2016-07-25 23:07:20 +0800

    author:ljc desription:for a test commit

---
+++ app/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // just for a test
+        setContentView(R.layout.activity_main);
+    }
+}
