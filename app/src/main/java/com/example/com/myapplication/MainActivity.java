79aa64b9b51f1000c85f4115b1986c96394f5a31
liujianchang <jianchangliu@yeah.net>
2016-07-25 23:33:11 +0800

    ljc for a test

--- app/src/main/java/com/example/com/myapplication/MainActivity.java
+++ app/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // just for a where the file is
+        // just for a test
         setContentView(R.layout.activity_main);
     }
 }
